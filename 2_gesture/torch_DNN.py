2248ff46b84799834e27bc9b91c03fca605388fe
zengbohan0217 <13811753844@126.com>
2020-11-18 10:53:03 +0800

    删掉不要的东西

--- 2_gesture/torch_DNN.py
+++ 2_gesture/torch_DNN.py
@@ -10,28 +10,6 @@ import json
 BATCH_SIZE = 64
 EPOCH = 100
 
-def get_dataset():
-    dataset_list = []  # size:n*6
-    character_dict = ["1", "2", "3", "4", "A", "B", "C", "D"]
-    for k in range(8):
-        with open('./data_set/{}.json'.format(character_dict[k]), 'r', encoding="UTF-8") as fp:
-            json_data = json.load(fp)
-            input_list = []
-            speed_list = []
-            angel_list = []
-            for need_key in json_data.keys():
-                speed_list.extend(json_data[need_key]["speed_accel"])
-                angel_list.extend(json_data[need_key]["angle_accel"])
-            for i in range(len(speed_list)):
-                data_part = []
-                label_part = [0] * 8
-                for j in range(3):
-                    data_part.append(speed_list[i][j])
-                    data_part.append(speed_list[i][j])
-                label_part[k] = 1
-                dataset_list.append((data_part, label_part))
-    random.shuffle(dataset_list)
-    return dataset_list
 
 class DNN(nn.Module):
     def __init__(self):
@@ -80,48 +58,6 @@ class dataset(Dataset):
             loader = json.load(fp)
         return loader
 
-def train(model, optimizer, train_set, epoch):
-    model.train()
-    point = 0
-    while point + BATCH_SIZE <= len(train_set):
-        part_data = train_set[point:point + BATCH_SIZE]
-        input_data = []
-        input_label = []
-        for data, label in part_data:
-            input_data.append(data)
-            input_label.append(label)
-        input_data = torch.Tensor(input_data)
-        input_label = torch.Tensor(input_label)
-        optimizer.zero_grad()
-        out_put = model(input_data)
-        labels_ = torch.max(input_label, 1)[1]
-        criteria = nn.CrossEntropyLoss()
-        loss = criteria(out_put, labels_)
-        loss.backward()
-        optimizer.step()
-        point = point + BATCH_SIZE
-    print("epoch {} finish loss is {}".format(epoch, loss.item()))
-
-def test(model, test_data, epoch):
-    model.eval()
-    correct = 0
-    with torch.no_grad():
-        point = 0
-        while point + BATCH_SIZE <= len(test_data):
-            part_data = test_data[point:point + BATCH_SIZE]
-            input_data = []
-            input_label = []
-            for data, label in part_data:
-                input_data.append(data)
-                input_label.append(label)
-            input_data = torch.Tensor(input_data)
-            input_label = torch.Tensor(input_label)
-            out_put = model(input_data)
-            labels_ = torch.max(input_label, 1)[1]
-            pred = out_put.max(1, keepdim=True)[1]
-            correct += pred.eq(labels_.view_as(pred)).sum().item()
-            point += BATCH_SIZE
-    print("Test set Accuracy: {:.2f}%\n".format(100. * correct / len(test_data)))
 
 def train_with_dataloader(model, optimizer, trainloader, epoch):
     model.train()
@@ -161,15 +97,12 @@ def test_with_dataloader(model, testloader):
 model = DNN()
 #optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 optimizer = optim.Adam(model.parameters(), lr=0.0005)
-#train_data_set = get_dataset()
 json_path = "./data_set/deal.json"       # 1120条
 train_data = dataset(json_path=json_path, start=0, length=700)
 train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
 test_data = dataset(json_path=json_path, start=700, length=300)
 test_loader = DataLoader(dataset=test_data, batch_size=8, shuffle=True)
 for epoch in range(0, EPOCH):
-    #train(model, optimizer, train_data_set, epoch)
-    #test(model, train_data_set, epoch)
     train_with_dataloader(model, optimizer, train_loader, epoch)
     test_with_dataloader(model, test_loader)
 
