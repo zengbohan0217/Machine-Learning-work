d15cff842a24175b1702714deaf28c5071434a90
zengbohan0217 <13811753844@126.com>
2021-01-06 16:43:45 +0800

    等高线便圆润了 可喜可贺

--- 1_UAVs/draw_background.py
+++ 1_UAVs/draw_background.py
@@ -8,11 +8,14 @@ def draw_print(input_path, input_enemy, target):
     for point in input_path:
         x.append(point[0])
         y.append(point[1])
-    x_e = np.arange(0, 20, 1)
-    y_e = np.arange(0, 20, 1)
+    # x_e = np.arange(0, 20, 1)
+    # y_e = np.arange(0, 20, 1)
+    n = 200
+    x_e = np.linspace(0, 20, n)
+    y_e = np.linspace(0, 20, n)
     X_e, Y_e = np.meshgrid(x_e, y_e)
     Z_e = X_e**2 + Y_e**2
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(8, 8))
     plt.contour(X_e, Y_e, Ematrix(input_enemy))
     plt.plot(x, y, color='r', marker='o', linestyle='dashed')
     plt.scatter(target[0], target[1], marker='x')
@@ -20,18 +23,18 @@ def draw_print(input_path, input_enemy, target):
     plt.show()
 
 def Ematrix(enemy_list):
-    E = [[0]*20 for _ in range(20)]
+    E = [[0]*200 for _ in range(200)]
     e_num = len(enemy_list)
     for i in range(e_num):
-        td = [[0]*20 for _ in range(20)]
-        for x in range(20):
-            for y in range(20):
+        td = [[0]*200 for _ in range(200)]
+        for x in range(200):
+            for y in range(200):
                 td[x][y] = get_dis.eucliDist_m([x,y],
-                                               [enemy_list[i][0], enemy_list[i][1]])
-        te = gaussmf(td, enemy_list[i][2], 0)
-        for x in range(20):
-            for y in range(20):
-                E[x][y] = 1-(1-E[x][y])*(1-te[x][y]/20)
+                                               [enemy_list[i][0]*10, enemy_list[i][1]*10])
+        te = gaussmf(td, enemy_list[i][2]*10, 0)
+        for x in range(200):
+            for y in range(200):
+                E[x][y] = 1-(1-E[x][y])*(1-te[x][y]/200)
     return E
 
 
